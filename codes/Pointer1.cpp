eacffd06aec0651e39b44891e5e2ba28c07c8150
Java coding <t9xy@hotmail.com>
2017-07-06 11:13:29 +0700

    Create Pointer1.cpp

---
+++ codes/Pointer1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+class Pointer1 {
+public:
+    int *Int_ptr;
+    int data[10];
+public:
+    Pointer1() {
+        Int_ptr = NULL;
+    };
+    ~Pointer1(){};
+    void init() {
+        int i;
+        Int_ptr = data;
+        for (i=0;i<10;i++) {
+            *Int_ptr = (i+1);
+            Int_ptr++;
+        }
+    }
+    void display() {
+        Int_ptr = data;
+        for (int i=0;i<10;i++) {
+            cout << (*Int_ptr) << endl;
+            Int_ptr++;
+        }
+    }
+
+};
+
+int main() {
+     Pointer1 pt1;
+     pt1.init();
+     pt1.display();
+
+     return 0;
+}
+
