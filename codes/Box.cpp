ba81963536a9b6afa7a7be41a540afb9c487d0d9
Java coding <t9xy@hotmail.com>
2017-07-06 08:56:01 +0700

    Update Box.cpp

--- codes/Box.cpp
+++ codes/Box.cpp
@@ -28,7 +28,7 @@ int main( ) {
    Box box1(5.0,6.0,7.0);        // Declare Box1 of type Box
    Box box2(10.0,12.0,13.0);        // Declare Box2 of type Box
 
-   cout.precision(5);
+   cout.precision(0);
    cout << "Volume of Box1 : " << fixed <<box1.area() <<endl;
 
    cout << "Volume of Box2 : " << fixed <<box2.area() <<endl;
